dc419a850daa83dc30300ccfb0846ece1697b71d
patrick ballantyne <P.J.Ballantyne@liverpool.ac.uk>
2021-03-10 15:03:40 +0000

    updates to workbooks and updated scripts to accompany

---
+++ Workbook-Part1.R
@@ -0,0 +1,265 @@
+knitr::opts_chunk$set(warning = FALSE, message = FALSE)
+library(knitr)
+purl("Workbook-Part1.Rmd", documentation = 0)
+
+library(sf)
+library(dplyr)
+library(tmap)
+#install.packages("data.table")
+library(data.table)
+library(hereR)
+
+#setwd("CDRC-Retail-Catchment-Training")
+
+## Read in the Retail Centre Boundaries 
+rc <- st_read("Data/LCR_Retail_Centres_2018.gpkg")
+
+## Print the first ten retail centres
+rc
+
+## Setup plotting
+tmap_mode("view")
+
+## Map Retail Centre Polygons for LCR
+tm_shape(rc) +
+  tm_fill(col = "orange") +
+  tm_text("rcName", size = 0.75)
+
+## Extract centroids
+rc_cent <- st_centroid(rc)
+
+## Map the centroids - note: tm_dots() is used as the object rc_cent contains point data (Retail Centre centroids)
+tm_shape(rc_cent) +
+  tm_dots(col = "orange") +
+  tm_text("rcName", size = 0.75)
+
+## Use mutate and case_when to create the new column - notice how ~ assigns the new values based on the condition
+rc_cent_nopipes <-  mutate(rc_cent, 
+   hierarchy = dplyr::case_when(n.comp.units < 50 & RetailPark == "N" ~ "tertiary",
+                              (n.comp.units >= 50 & n.comp.units < 100) | RetailPark == "Y" ~ "secondary",
+                                n.comp.units >= 100 ~ "primary")) 
+
+## Use select to extract the id, n.units, n.comp.units and hierarchy columns
+rc_cent_nopipes <- select(rc_cent_nopipes, rcID, rcName, n.units, n.comp.units, RetailPark, hierarchy)
+
+## Sort by hierarchy
+rc_cent_nopipes <- arrange(rc_cent_nopipes, hierarchy)
+
+## Use pipes to create the new hierarchy column and then select the other columns we are interested in
+rc_cent <- rc_cent %>% 
+  mutate(hierarchy = dplyr::case_when(n.comp.units < 50 & RetailPark == "N" ~ "tertiary",
+             (n.comp.units >= 50 & n.comp.units < 100) | RetailPark == "Y" ~ "secondary",
+              n.comp.units >= 100 ~ "primary")) %>%
+  select(rcID, rcName, n.units, n.comp.units, RetailPark, hierarchy) %>%
+  arrange(hierarchy)
+
+## Without pipes
+rc_cent_nopipes
+
+## With pipes
+rc_cent
+
+## View rc_cent
+View(rc_cent)
+
+## Extract a 1500m buffer for each Retail Centre
+buffer1.5km <- st_buffer(rc_cent, 1500)
+
+## Map the buffers
+tm_shape(buffer1.5km) + ## Plot the buffers
+  tm_fill(col = "orange", alpha = 0.3) +
+  tm_shape(rc_cent) + ## Overlay the centroids
+  tm_dots(col = "orange") +
+  tm_text("rcName", size = 0.75)
+
+## Run this chunk to save the function to your environment
+get_primary_buffer <- function(centroids) {
+  
+  ## Extracts centroids of 'primary' Retail Centres
+  rc_primary <- filter(centroids, hierarchy == "primary")
+  
+  ## Constructs a 5000m buffer
+  primary_buffer <- st_buffer(rc_primary, dist = 5000)
+  
+  ## Return the 5000m buffer
+  return(primary_buffer)
+}
+
+## Get primary buffers for LCR Retail Centres
+pbuffer <- get_primary_buffer(rc_cent)
+
+## Plot the primary buffers and all the centroids, to check only primary centres have catchments
+tm_shape(pbuffer) +
+  tm_fill(col = "orange", alpha = 0.3) +
+  tm_shape(rc_cent) +
+  tm_dots(col = "orange") +
+  tm_text("rcName", size = 0.75)
+
+## Run this chunk to save the function to your environment
+get_buffer <- function(centroids, primary_dist = 5000, secondary_dist = 3000, tertiary_dist = 1500) {
+  
+  ## Split up the Retail Centres based on hierarchy
+  rc_primary <- filter(centroids, hierarchy == "primary")
+  rc_secondary <- filter(centroids, hierarchy == "secondary")
+  rc_tertiary <- filter(centroids, hierarchy == "tertiary")
+  
+  ## Run the buffer for the different Retail Centre hierarchies separately
+  primary_buffer <- st_buffer(rc_primary, dist = primary_dist)
+  secondary_buffer <- st_buffer(rc_secondary, dist = secondary_dist)
+  tertiary_buffer <- st_buffer(rc_tertiary, dist = tertiary_dist)
+  
+  ## Join together
+  buffer <- rbind(primary_buffer, secondary_buffer, tertiary_buffer)
+  return(buffer) ## Return
+}
+
+## Run the function
+hbuffer <- get_buffer(rc_cent, primary_dist = 5000, secondary_dist = 3000, tertiary_dist = 1500)
+
+tm_shape(hbuffer)+ ## Plot the varying fixed-ring buffers
+  tm_fill(col = "hierarchy", palette = c("yellow", "orange", "red"), alpha = 0.5) + # Setting col to 'hierarchy' tells tmap to generate a different colour buffer for each value in the hierarchy column
+  tm_shape(rc_cent) + ## Overlay the centroids
+  tm_dots(col = "orange", alpha = 0.75) +
+  tm_text("rcName", size = 0.75)
+
+## Set API key
+#set_key("insert-key-here")
+
+## Set API key
+set_key("GaVydNx-bQsR-r5QNoFveZsEvp43DD1kPAwRtRZ22I0")
+
+## Extract Liverpool City Retail Centre - the first in our dataset
+rc_a <- rc_cent[1, ]
+
+## Extract the 10-minute driving catchment
+iso_a <- isoline(rc_a, range = (15 * 60), range_type = "time", transport_mode = "car")
+
+## Map the drive-time catchment for the first Retail Centre
+tm_shape(iso_a) +
+  tm_fill(col = "orange", alpha = 0.5) +
+  tm_shape(rc_a) +
+  tm_dots(col = "orange") +
+  tm_text("rcName", size = 0.75)
+
+## Extract the 5-minute catchment for every Retail Centre in LCR
+iso <- isoline(rc_cent, range = (5 * 60), range_type = "time", transport_mode = "car", aggregate = FALSE)
+
+## Map the 5-minute drive-time catchments for LCR Retail Centres
+tm_shape(iso) +
+  tm_fill(col = "orange", alpha = 0.3) +
+  tm_shape(rc_cent) +
+  tm_dots(col = "orange") +
+  tm_text("rcName", size = 0.75)
+
+## First set up the date & time you are interested in - e.g. Friday 5th Mar - 5pm 
+friday5th <- as.POSIXct("2020-03-05 17:30:00 CET", tz = "Europe/Zurich")
+## Build the catchment for Friday (rush hour)
+friday_iso <- isoline(rc_a, range = (10 * 60), range_type = "time", transport_mode = "car",
+                      datetime = friday5th)
+
+## First set up the date & time you are interested in - e.g. Sunday 7th - 5am
+sunday7th <- as.POSIXct("2020-03-07 05:00:00 CET", tz = "Europe/Zurich")
+## Build the catchment for Friday (rush hour)
+sunday_iso <- isoline(rc_a, range = (10 * 60), range_type = "time", transport_mode = "car",
+                      datetime = sunday7th)
+
+## Make the Maps
+p1 <- tm_shape(friday_iso) +
+  tm_fill(col = "orange", alpha = 0.75) +
+  tm_shape(rc_a) +
+  tm_dots(col = "orange") +
+  tm_text("rcName", size = 0.75)
+p2 <- tm_shape(sunday_iso) +
+  tm_fill(col = "orange", alpha = 0.75) +
+  tm_shape(rc_a) +
+  tm_dots(col = "orange") +
+  tm_text("rcName", size = 0.75)
+
+
+## Plot them side-by-side
+tmap_arrange(p1, p2, ncol = 2)
+
+## Function to get drive-time catchments for the primary Retail Centres
+get_primary_drive_time <- function(centroids, dist = 15, range_type = "time", transport_mode = "car") {
+  
+  ## Filter the centroids to extract the primary centres
+  rc_primary <- filter(centroids, hierarchy == "primary")
+  
+  ## Build the drive-time catchment
+  primary_drive_time <- isoline(rc_primary, range = (dist * 60), 
+                                range_type = range_type, transport_mode = transport_mode, aggregate = FALSE)
+  
+  ## Clean up the isoline - join on the Retail Centre information
+  rc_primary <- rc_primary %>%
+    as.data.frame() %>% 
+    select(rcID, n.units, n.comp.units, hierarchy) %>%
+    bind_cols(primary_drive_time) %>% ## Equivalent of cbind(), but for piping
+    st_as_sf() ## Ensures final object is SF not dataframe
+  return(rc_primary)
+}
+
+## Get catchments for primary centres
+primary_iso <- get_primary_drive_time(rc_cent, dist = 15, range_type = "time", transport_mode =  "car")
+
+## Map the primary drive-time catchments
+tm_shape(primary_iso) +
+  tm_fill(col = "orange", alpha = 0.5) +
+  tm_shape(rc_cent) +
+  tm_dots(col = "orange") +
+  tm_text("rcName", size = 0.75)
+
+## Run this chunk to save this function to your environment
+get_drive_time <- function(centroids, primary_dist = 15, secondary_dist = 10, tertiary_dist = 5, 
+                           range_type = "time", transport_mode = "car") {
+  
+  ## Split up the Retail Centres based on hierarchy
+  rc_primary <- filter(centroids, hierarchy == "primary")
+  rc_secondary <- filter(centroids, hierarchy == "secondary")
+  rc_tertiary <- filter(centroids, hierarchy == "tertiary")
+  
+  ## Delineate the isolines for the different Retail Centre hierarchies separately
+  primary_drive_time <- isoline(rc_primary, range = (primary_dist * 60),
+                                range_type = range_type, transport_mode = transport_mode, aggregate = FALSE)
+  secondary_drive_time <- isoline(rc_secondary, range = (secondary_dist * 60),
+                                  range_type = range_type, transport_mode = transport_mode, aggregate = FALSE)
+  tertiary_drive_time <- isoline(rc_tertiary, range = (tertiary_dist * 60),
+                                 range_type = range_type, transport_mode = transport_mode, aggregate = FALSE)
+  
+  ## Join the Retail Centre info onto each set of catchments
+  primary <- rc_primary %>%
+    as.data.frame() %>%
+    select(rcID, rcName, n.units, n.comp.units, hierarchy) %>%
+    bind_cols(primary_drive_time)
+  secondary <- rc_secondary %>%
+    as.data.frame() %>%
+    select(rcID, rcName, n.units, n.comp.units, hierarchy) %>%
+    bind_cols(secondary_drive_time)
+  tertiary <- rc_tertiary %>%
+    as.data.frame() %>%
+    select(rcID, rcName, n.units, n.comp.units, hierarchy) %>%
+    bind_cols(tertiary_drive_time)
+  
+  ## Join catchments together
+  isolines <- st_as_sf(rbind(primary, secondary, tertiary))
+  return(isolines)
+}
+
+## Extract the hierarchical drive time catchments for LCR Retail Centres
+iso_hierarchy <- get_drive_time(rc_cent, primary_dist = 15, secondary_dist = 10, tertiary_dist = 5, 
+                                range_type = "time", transport_mode = "car")
+
+tm_shape(iso_hierarchy) + ## Plot the hierarchical drive-time catchments 
+  tm_fill(col = "hierarchy", palette = c("yellow", "orange", "red"), alpha = 0.5) + ## Setting col = 'hierarchy' tells tmap to plot a different colour for each value of hierarchy
+  tm_shape(rc_cent) + ## Overlay the centroids
+  tm_dots(col = "orange" ) +
+  tm_text("rcName", size = 0.75)
+
+## Filter to catchments of interest
+iso_hierarchy %>%
+  filter(rcName== "Liverpool City") %>%
+  tm_shape() + ## Notice how tm_shape can be left empty as you have piped directly from the iso_hierarchy object
+  tm_fill(col = "orange", alpha = 0.5) +
+  tm_text("rcName", size = 0.75)
+
+## Write out to Data folder
+# st_write(rc_cent, "Data/Part2_Retail_Centres.gpkg")
---
+++ Workbook-Part2.R
@@ -0,0 +1,209 @@
+knitr::opts_chunk$set(warning = FALSE, message = FALSE)
+library(knitr)
+purl("Workbook-Part2.Rmd", documentation = 0)
+
+library(sf)
+library(dplyr)
+library(tmap)
+tmap_mode("view") ## Interactive Mapping
+
+#install.packages("FNN")
+library(rgdal)
+library(rgeos)
+library(igraph)
+library(FNN)
+
+#setwd("CDRC-Retail-Catchment-Training")
+
+## Save huff-tools.R functions to memory
+source("Scripts/huff-tools.R")
+
+## Read in the LCR Retail Centres (with Hierarchy from Part 1)
+rc <- st_read("Data/Part2_Retail_Centres.gpkg")
+
+## Read in the Liverpool City Region LSOAs
+lsoa <- st_read("Data/LCR_LSOA.gpkg")
+
+## Plot the LSOAs for Liverpool City Region 
+tm_shape(lsoa) +
+  tm_fill(alpha = 0.75) +
+  tm_borders(col = "black", lwd = 0.25, alpha = 0.5)
+
+## Read in Distances.csv
+distances <- read.csv("Data/Distances.csv")
+
+## Use head() to print the first few rows of any data frame or tabular dataset
+head(distances)
+
+## Joining rc and distances together, and then tidy up the object (data.frame, selecting columns)
+huff_input <- rc %>%
+  inner_join(distances) %>%
+  as.data.frame() %>%
+  select(rcID, rcName,n.comp.units, RetailPark, hierarchy, lsoa11cd, distance)
+
+## Joing rc and distances together, and then tidy up the object (data.frame, selecting columns) NO PIPES
+# huff_input <- inner_join(rc, distances)
+# huff_input <- as.data.frame(huff_input)
+# huff_input <- select(huff_input, rcID, rcName, n.comp.units, hierarchy, lsoa11cd, distance)
+
+## use head() to print the first few rows of any data frame or tabular dataset
+head(huff_input)
+
+## Create attractiveness score column and modify based on whether Retail Centre is in a Retail Park or not
+huff_input <- huff_input %>%
+  mutate(attr_score = n.comp.units) %>%
+  mutate(attr_score, ifelse(RetailPark == "N", attr_score,
+                            ifelse(RetailPark == "Y", attr_score * 2, 0))) %>%
+  select(-c(attr_score)) %>%
+  rename(attr_score = 8)
+
+## Create column called beta, where the value = 2 for each Retail Centre
+huff_input$beta <- 2
+
+## Create column called alpha, where the value = 1 for each Retail Centre
+huff_input$alpha <- 1
+
+## head() displays the first few rows of any data frame or tabular object
+head(huff_input)
+
+## Select the columns we need for the Huff Model
+huff_default <- huff_input %>%
+  select(rcName, attr_score, lsoa11cd, distance, alpha, beta)
+
+## Fit the huff model, setting alpha to 1 and beta to 2 - these are the default parameters
+huff_probs <- huff_basic(destinations_name = huff_default$rcName,
+                         destinations_attractiveness = huff_default$attr_score,
+                         origins_name = huff_default$lsoa11cd,
+                         distance = huff_default$distance,
+                         alpha = huff_default$alpha,
+                         beta = huff_default$beta)
+
+## head() displays the first few rows of any data frame or tabular object
+head(huff_probs)
+
+## Extract the highest huff probability in each LSOA
+top_probs <- select_by_probs(huff_probs, 1)
+
+## head displays the top few rows of a dataframe or any tabular object
+head(top_probs)
+
+## Remove LSOAs with huff probs < 60%
+top_probs_60 <- filter(top_probs, huff_probability >= 0.6)
+
+top_probs_40 <- filter(top_probs, huff_probability >= 0.4)
+
+## Create beta parameters, using mutate() and case_when()
+huff_input <- huff_input %>%
+  dplyr::mutate(beta = dplyr::case_when(hierarchy == "primary" ~ 1.9,
+                                          hierarchy == "secondary" ~ 2.0,
+                                            hierarchy == "tertiary" ~ 2.1))
+
+## Create beta parameters, using mutate() and case_when() NO PIPES
+# huff_input <- mutate(huff_input, beta = case_when(hierarchy == "primary" ~ 1.9,
+#                                           hierarchy == "secondary" ~ 2.0,
+#                                             hierarchy == "tertiary" ~ 2.1) )
+
+## use head() to print the first few rows of any data frame or tabular dataset
+head(huff_input)
+
+## Fit the huff model, setting alpha to 1 and beta the the beta column, containing the values we set earlier
+hierarchy_huff_probs <- huff_basic(destinations_name = huff_input$rcName,
+                         destinations_attractiveness = huff_input$attr_score,
+                         origins_name = huff_input$lsoa11cd,
+                         distance = huff_input$distance,
+                         alpha = huff_input$alpha,
+                         beta = huff_input$beta)
+
+## Extract the highest huff probability in each LSOA
+hierarchy_top_probs <- select_by_probs(hierarchy_huff_probs, 1)
+
+## Remove those with < 60% huff probability
+hierarchy_top_probs_60 <- filter(hierarchy_top_probs, huff_probability >= 0.6)
+## Remove those with < 40% huff probability
+hierarchy_top_probs_40 <- filter(hierarchy_top_probs, huff_probability >= 0.4)
+
+# head() displays the first few rows of a data frame or tabular object
+head(hierarchy_top_probs_60)
+
+## Tidy up top_probs 60 & 40
+top_probs_60 <- top_probs_60 %>%
+  rename(lsoa11cd = origins_name, rcName = destinations_name) %>%
+  select(lsoa11cd, rcName, huff_probability)
+top_probs_40 <- top_probs_40 %>%
+  rename(lsoa11cd = origins_name, rcName = destinations_name) %>%
+  select(lsoa11cd, rcName, huff_probability)
+
+## Tidy up the output of the huff model using different beta values for hierarchy PIPED
+hierarchy_top_probs_60 <-hierarchy_top_probs_60%>%
+  rename(lsoa11cd = origins_name, rcName = destinations_name) %>%
+  select(lsoa11cd, rcName, huff_probability)
+hierarchy_top_probs_40 <-hierarchy_top_probs_40%>%
+  rename(lsoa11cd = origins_name, rcName = destinations_name) %>%
+  select(lsoa11cd, rcName, huff_probability)
+
+## Merge the results of the huff model with default parameters onto the LSOAs - 40 & 60%
+lcr_huff_60 <- merge(lsoa, top_probs_60, by = "lsoa11cd")
+lcr_huff_40 <- merge(lsoa, top_probs_40, by = "lsoa11cd")
+
+## Merge on the results of the huff model accounting for hierarchy with beta values onto the LSOAs - 40 & 60%
+lcr_hierarchy_huff_60 <- merge(lsoa, hierarchy_top_probs_60, by = "lsoa11cd")
+lcr_hierarchy_huff_40 <- merge(lsoa, hierarchy_top_probs_40, by = "lsoa11cd")
+
+## Huff probabilities (default parameters)
+head(lcr_huff_60)
+
+## Huff probabilities (beta values set to reflect hierarchy)
+head(lcr_hierarchy_huff_60)
+
+## Map the allocation of LSOAs to Retail Centres - notice the additional arguments in tm_layout() to move the legend outside the map frame, and tm_borders() to show LSOA boundaries clearly
+## Plot the centroids over these too
+tm_shape(lcr_huff_60) +
+  tm_fill(col = "rcName", alpha = 0.4) +
+  tm_borders(col = "black", lwd = 0.25, alpha = 0.2) +
+  tm_layout(legend.outside = TRUE, legend.outside.position = "right") +
+  tm_shape(rc) +
+  tm_dots(col = "orange", size = 0.05) +
+  tm_text("rcName")
+
+## Map the allocation of LSOAs to Retail Centres - notice the additional arguments in tm_layout() to move the legend outside the map frame, and tm_borders() to show LSOA boundaries clearly
+tm_shape(lcr_hierarchy_huff_60) +
+  tm_fill(col = "rcName", alpha = 0.4) +
+  tm_borders(col = "black", lwd = 0.25, alpha = 0.2) +
+  tm_layout(legend.outside = TRUE, legend.outside.position = "right") +
+  tm_shape(rc) +
+  tm_dots(col = "orange", size = 0.05) +
+  tm_text("rcName")
+
+## Extract LSOAs and catchment for the Huyton Retail Centre
+huyton <- filter(lcr_huff_60, rcName == "Huyton")
+
+## Extract the Huyton Retail Centre Centroid
+huyton_rc <- filter(rc, rcName == "Huyton")
+
+## Map the Huyton Retail Centre and Huff Catchment
+tm_shape(huyton) +
+  tm_fill(col = "orange", alpha = 0.5) +
+  tm_borders(col = "black", lwd = 0.25, alpha = 0.2) +
+  tm_shape(huyton_rc) +
+  tm_dots(col = "orange", size = 0.25) +
+  tm_text("rcName", size = 0.75)
+
+## Extract Huff catchments for each Retail Centre
+catchments <- lcr_huff_60 %>%
+  group_by(rcName) %>%
+  summarise(n.lsoa = n())
+
+## head() displays the first few rows of any dataframe or tabular object
+head(catchments)
+
+## Map the Huff Catchments for the Retail Centres and overlay Retail Centres
+tm_shape(catchments) +
+  tm_fill(col = "rcName", alpha = 0.4) +
+  tm_borders(col = "black", lwd = 0.25, alpha = 0.2) +
+  tm_shape(rc) +
+  tm_dots(size = 0.015, alpha = 0.1, col = "orange") +
+  tm_text("rcName", size = 0.75)+
+  tm_layout(legend.outside = TRUE, legend.outside.position = "right")
+
+## Write out your catchments
+# st_write(catchments, "Data/LCR_RC_Huff_Catchments.gpkg")
